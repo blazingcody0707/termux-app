2d43e2efcac89936c19d104fe0952894a82ccb6d
Chen Jiaxin <chenjiaxin@shopee.com>
2025-02-05 19:22:42 +0800

    termux display bridge implementation

--- termux-display/src/main/cpp/termux-display-client/include/client.h
+++ termux-display/src/main/cpp/termux-display-client/include/client.h
@@ -1,19 +1,25 @@
 #pragma once
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include "InputEvent.h"
 #include "types.h"
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 void DisplayClientInit(uint32_t width, uint32_t height, uint32_t channel);
 
 void DisplayClientStart();
 
 void DisplayDraw(const uint8_t *data);
 
+void BeginDisplayDraw(const uint8_t *data);
+
+void EndDisplayDraw();
+
 void InputInit(InputHandler handler);
+
+#ifdef __cplusplus
+}
+#endif
